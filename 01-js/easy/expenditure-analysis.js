837745bc6f97a3977d8fe140783f88e3fb2ee0ea
bhavika2203 <138359620+bhavika2203@users.noreply.github.com>
2025-01-10 14:39:36 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -3,6 +3,7 @@
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
+  calculate the expenses
 
   Once you've implemented the logic, test your code by running
   - `npm run test-expenditure-analysis`
